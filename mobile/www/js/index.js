a1703803525de8cfe02a93fc0d5517546f11cfe9
Eduardo Mangeli <eduardo@mangeli.com.br>
2016-03-14 23:25:35 -0300

    mobile example

--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -1,51 +1,39 @@
+var app={
+
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+    Application constructor
  */
-var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
+        this.extraConfig();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+/*
+    bind any events that are required on startup to listeners:
+*/
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+
+    
+/*
+    this runs when the device is ready for user interaction:
+*/
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+/*
+    appends @error to the message div:
+*/
+    showError: function(error) {
+        app.display(error);
+    },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    extraConfig : function(){
+        //initialize panel
+        $(function () { $("[data-role=panel]").panel().enhanceWithin(); });
+   }
 
-        console.log('Received Event: ' + id);
-    }
-};
 
-app.initialize();
+};      // end of app
+
+$( document ).ready(function(){app.initialize()});
