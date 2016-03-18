0be56f496c0b7d81a1aba7a4ce51d9eaebab1fec
Fabrício Raphael Silva Pereira <fabriciorsf@gmail.com>
2016-03-17 22:44:16 -0300

    code formatting

--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -1,96 +1,95 @@
-var app={
-		
-	versao: "2.0.0",
-	
+var app = {
+
+	versao : "2.0.0",
+
 	user_admin : {
-		usuario: 'admin',
-		senha: "123"
-		
+		usuario : 'admin',
+		senha : "123"
 	},
 
-	login: function() {
+	login : function() {
 		var usuario = $("#usuario").val();
 		var senha = $("#senha").val();
 		if (usuario == app.user_admin.usuario && senha == app.user_admin.senha) {
-			//navega para págine e executa o script de configuração depois do carregamento
-         app.trocaPagina("menu.html", pages_config.menu)
-			
+			// navega para págine e executa o script de configuração depois do carregamento
+			app.trocaPagina("menu.html", pages_config.menu)
+
 		} else {
-			//TODO Trocar por um popup "mais elegante"
-         alert("usuário e senha informados não estão cadastrados no sistema");               
-      }
+			// TODO Trocar por um popup "mais elegante"
+			alert("usuário e senha informados não estão cadastrados no sistema");
+		}
 	},
-	
-/*
-    Application constructor
- */
-    initialize: function() {
-        this.bindEvents();
-        this.extraConfig();
-    },
-/*
-    bind any events that are required on startup to listeners:
-*/
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
 
-    
-/*
-    this runs when the device is ready for user interaction:
-*/
-    onDeviceReady: function() {
+	/*
+	 * Application constructor
+	 */
+	initialize : function() {
+		this.bindEvents();
+		this.extraConfig();
+	},
 
-    },
-/*
-    appends @error to the message div:
-*/
-    showError: function(error) {
-        app.display(error);
-    },
+	/*
+	 * bind any events that are required on startup to listeners:
+	 */
+	bindEvents : function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
 
-    extraConfig : function(){
-        //initialize panel
-        $(function () { $("[data-role=panel]").panel().enhanceWithin(); });
-        $("#versao").html(this.versao);
-        $("#entrar").click(this.login);
-   },
+	/*
+	 * this runs when the device is ready for user interaction:
+	 */
+	onDeviceReady : function() {
 
-   trocaPagina : function (pagina, configs){
-      $(":mobile-pagecontainer").on("pagecontainershow", configs);
-      $(":mobile-pagecontainer").pagecontainer("change", pagina);
-   }
+	},
+	/*
+	 * appends @error to the message div:
+	 */
+	showError : function(error) {
+		app.display(error);
+	},
 
+	extraConfig : function() {
+		// initialize panel
+		$(function() {
+			$("[data-role=panel]").panel().enhanceWithin();
+		});
+		$("#versao").html(this.versao);
+		$("#entrar").click(this.login);
+	},
 
-};      // end of app
+	trocaPagina : function(pagina, configs) {
+		$(":mobile-pagecontainer").on("pagecontainershow", configs);
+		$(":mobile-pagecontainer").pagecontainer("change", pagina);
+	}
+
+}; // end of app
 
 var pages_config = {
 
-   menu : function(){
-		$('#menu_nova_pesquisa').click( function(){
-         app.trocaPagina("selecionar_tipo.html", pages_config.selecionar_tipo);
-      });
-   },
-   
-   selecionar_tipo : function() {
-	   $(".img_sel").each(function(){
-		   $(this).click( function(){
-			   veiculo_confirmar = $(this).attr('id');
-			   app.trocaPagina("confirmar_veiculo.html",pages_config.confirmar_veiculo);
-		      })
-	   })
+	menu : function() {
+		$('#menu_nova_pesquisa').click(function() {
+			app.trocaPagina("selecionar_tipo.html", pages_config.selecionar_tipo);
+		});
+	},
 
-   },
-   
-   confirmar_veiculo : function() {
-	   var image_path = "img/tipoVeiculo/" + veiculo_confirmar + ".png";
-	   $("#img_confirmar_veiculo").attr('src',image_path);
-	   //$("#conf_veic_nao").click();
-   },
-	
+	selecionar_tipo : function() {
+		$(".img_sel").each(function() {
+			$(this).click(function() {
+				veiculo_confirmar = $(this).attr('id');
+				app.trocaPagina("confirmar_veiculo.html", pages_config.confirmar_veiculo);
+			})
+		})
+	},
 
-   nova_pesquisa : function(){}
- 
+	confirmar_veiculo : function() {
+		var image_path = "img/tipoVeiculo/" + veiculo_confirmar + ".png";
+		$("#img_confirmar_veiculo").attr('src', image_path);
+		// $("#conf_veic_nao").click();
+	},
+
+	nova_pesquisa : function() {
+	}
+
 };
 
 var veiculo_confirmar = {};
@@ -98,4 +97,6 @@ var veiculo_confirmar = {};
 // Registro do momento
 var registro = {};
 
-$( document ).ready(function(){app.initialize()});
+$(document).ready(function() {
+	app.initialize()
+});
