a19d51f0a42f43833c48f5d8fac4c119ca87eef5
Eduardo Mangeli <eduardo@mangeli.com.br>
2016-03-19 18:11:12 -0300

    country listing, views in a separate folder. 'identificacao_visual.html' of 'simples' process

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -31,6 +31,32 @@ var controllers = {
                 registro = {};
                 app.trocaPagina('selecionar_tipo.html', controllers.selecionar_tipo);
             });
+            $("#conf_veic_sim").click(function(){
+                
+                app.trocaPagina('views/' + registro.classeVeiculo + '/identificacao_visual.html',
+                                controllers['identificacao_visual_' + registro.classeVeiculo]);
+            })
+            
+    },
+    
+    identificacao_visual_simples : function(){
+       var insert_paises = ""; 
+       
+       //preenchendo paises
+        $.each(paises.listados(), function(index, item){
+           insert_paises += "<option value='" + item + "'>" + item +"</option>\n";
+       });
+       $("#pais_simples").html(insert_paises);
+       
+        //progresso
+       $('#reboque_simples_sim').click(function(){$('#grupo_placa_estrangeira_simples').show()});
+       $('#reboque_simples_nao').click(function(){$('#grupo_placa_estrangeira_simples').show()});
+       
+       $('#placa_estrangeira_simples_sim').click(function(){$('#grupo_pais_simples').show()});
+       $('#placa_estrangeira_simples_nao').click(function(){
+                                                $('#grupo_pais_simples').hide()
+                                                });
+       
     },
 
     nova_pesquisa : function() {
