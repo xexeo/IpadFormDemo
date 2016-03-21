6947ed8b80fb404fb226f36bff4acf7832c3d386
Marcelo Areas <mareas@gmail.com>
2016-03-21 18:41:50 -0300

    Preenchimento de combustível para moto; padronização de nome de variáveis; eventos na tela de caracterização do veículo

--- mobile/www/controllers/caracterizacao_simples.js
+++ mobile/www/controllers/caracterizacao_simples.js
@@ -16,29 +16,83 @@ controllers.caracterizacao_simples = {
     },
     
     fillSelect : function(){
-        var anos = "<option value='-1'>Selecione</option>\n";
-        
+    	
+    	// Preenche combos
+    	var anos = "<option value='-1'>Selecione</option>\n";
         for (var ano=2016;ano>1899;ano--){
             anos += "<option value='" + String(ano) + "'>" + String(ano) + "</option>\n";
         }
-        
         $("#ano_simples").html(anos).selectmenu("refresh", true);
         
-        //TODO: implementar restrição a combustíveis de moto - necessita códigos veículos leves
-        
-        var combustiveis = ['Álcool/Etanol', 'Bi-Combustível/Etanol', 'Diesel',
-                            'Gasolina', 'GNV/Gás Natural', 'Híbrido'
-                            ];
+        var propriedades = ['Próprio', 'Alugado/Fretado', 'Empresa', 'Taxi', 'Serviços Públicos', 'Outros'];
+        var insert_propriedades = "<option value='-1'>Selecione</option>\n";
+        $.each(propriedades, function(index, item) {
+        	insert_propriedades += "<option value='" + item + "'>" + item + "</option>\n";
+        })
+        $("#propriedade_simples").html(insert_propriedades).selectmenu("refresh", true);
+
+        var combustiveis = ['Álcool/Etanol', 'Bi-Combustível/Etanol', 'Diesel', 'Gasolina', 'GNV/Gás Natural', 'Híbrido'];
+        // Se o veículo leve for moto, as opções para combustível são diferentes
+        if (registro.codVeiculo == 'tpVL03') {
+        	combustiveis = ['Bi-Combustível/Etanol', 'Gasolina'];
+		}
         var insert_combustiveis = "<option value='-1'>Selecione</option>\n";
-        
         $.each(combustiveis, function(index, item){
             insert_combustiveis += "<option value='" + item + "'>" + item + "</option>\n"; 
         })
-        
         $("#combustivel_simples").html(insert_combustiveis).selectmenu("refresh", true);
+        
+        
+        // Valores anteriores e eventos
+        if (registro.ano != null) {
+        	$("#ano_simples option[value='" + registro.ano + "'").attr('selected', true);
+        	$("#grupo_propriedade_simples").show();
+		}
+        $("select#ano_simples").selectmenu("refresh", true);
+        
+        if (registro.propriedade != null) {
+        	$("#propriedade_simples option[value='" + registro.propriedade + "'").attr('selected', true);
+        	$("#grupo_combustivel_simples").show();
+		}
+        $("select#propriedade_simples").selectmenu("refresh", true);
+        
+        if (registro.combustivel!= null) {
+        	$("#combustivel_simples option[value='" + registro.combustivel + "'").attr('selected', true);
+        	$("#grupo_caracterizacao_simples_avancar").show();
+		}
+        $("select#combustivel_simples").selectmenu("refresh", true);
+        
+        
+        // Progresso
+        $('#ano_simples').change(function(){
+            if(Number($(this).val()) != -1){
+                app.setAtributo('ano', $(this).val());
+                $("#grupo_propriedade_simples").show();
+            } else {
+                $("#grupo_propriedade_simples").hide();
+                app.setAtributo('pais', null);
+            }
+        });
+        $('#propriedade_simples').change(function(){
+            if(Number($(this).val()) != -1){
+                app.setAtributo('propriedade', $(this).val());
+                $("#grupo_combustivel_simples").show();
+            } else {
+                $("#grupo_combustivel_simples").hide();
+                app.setAtributo('propriedade', null);
+            }
+        });
+        $('#combustivel_simples').change(function(){
+            if(Number($(this).val()) != -1){
+                app.setAtributo('combustivel', $(this).val());
+                $("#grupo_caracterizacao_simples_avancar").show();
+            } else {
+                $("#grupo_caracterizacao_simples_avancar").hide();
+                app.setAtributo('combustivel', null);
+            }
+        });
+        
     }
-    
-    
     
 };
 
--- mobile/www/controllers/identificacao_visual_simples.js
+++ mobile/www/controllers/identificacao_visual_simples.js
@@ -1,15 +1,21 @@
 controllers.identificacao_visual_simples = {
   config : function(){
-      //é preciso carregar o controller anterior
-      //$('#identificacao_visual_simples_back').click($.mobile.back);
+      var me = controllers.identificacao_visual_simples;
+      me.fillSelect();
+      me.buttons();
+  },
+  
+  buttons : function(){
       $('#identificacao_visual_simples_back').click( function(){
-            app.trocaPagina('../confirmar_veiculo.html', controllers.confirmar_veiculo);
-        });
-        
-       $("#identificacao_visual_simples_avancar").click(function(){
-           app.trocaPagina('caracterizacao_simples.html', controllers.caracterizacao_simples);
-       })
+          app.trocaPagina('../confirmar_veiculo.html', controllers.confirmar_veiculo);
+      });
       
+     $("#identificacao_visual_simples_avancar").click(function(){
+         app.trocaPagina('caracterizacao_simples.html', controllers.caracterizacao_simples);
+     })
+  },
+  
+  fillSelect : function(){
        var insert_paises = "<option value='-1'>Selecione</option>\n"; 
        
        //preenchendo paises
@@ -34,7 +40,7 @@ controllers.identificacao_visual_simples = {
        if (registro.placa_estrangeira != null && registro.placa_estrangeira){
             $('#placa_estrangeira_simples_sim').prop('checked', true).checkboxradio('refresh');;
             $('#grupo_pais_simples').show();
-            $("#grupo_identificacao_visual_avancar_simples").hide();
+            $("#grupo_identificacao_visual_simples_avancar").hide();
        } else if (registro.placa_estrangeira != null && !registro.placa_estrangeira){
             $('#placa_estrangeira_simples_nao').prop('checked', true).checkboxradio('refresh');;
             $('#grupo_pais_simples').hide();
@@ -44,14 +50,14 @@ controllers.identificacao_visual_simples = {
            $('#placa_estrangeira_simples_sim').prop('checked', false).checkboxradio('refresh');;
            $('#grupo_pais_simples').hide();
            $("#pais_simples option[value='-1']").attr('selected', true);
-           $("#grupo_identificacao_visual_avancar_simples").hide();
+           $("#grupo_identificacao_visual_simples_avancar").hide();
        }
        
        if (registro.pais != null){
            $("#pais_simples option[value='" + registro.pais + "'").attr('selected', true);
-           $("#grupo_identificacao_visual_avancar_simples").show();
+           $("#grupo_identificacao_visual_simples_avancar").show();
        } else if (registro.placa_estrangeira != null && !registro.placa_estrangeira){
-           $("#grupo_identificacao_visual_avancar_simples").show();
+           $("#grupo_identificacao_visual_simples_avancar").show();
        }
        
        $("select#pais_simples").selectmenu("refresh", true);
@@ -61,35 +67,39 @@ controllers.identificacao_visual_simples = {
        //progresso
        $('#reboque_simples_sim').click(function(){
            $('#grupo_placa_estrangeira_simples').show();
-           app.setAtributo('reboque', 'true');
+           app.setAtributo('reboque', true);
        });
        $('#reboque_simples_nao').click(function(){
            $('#grupo_placa_estrangeira_simples').show();
-           app.setAtributo('reboque', 'false');
+           app.setAtributo('reboque', false);
        });
        
        $('#placa_estrangeira_simples_sim').click(function(){
            $('#grupo_pais_simples').show();
-           c
+           app.setAtributo('placa_estrangeira', true);
            
            if(Number($('#pais_simples').val()) == -1){
-               $("#grupo_identificacao_visual_avancar_simples").hide();
+               $("#grupo_identificacao_visual_simples_avancar").hide();
            }
            
        });
        $('#placa_estrangeira_simples_nao').click(function(){
             $('#grupo_pais_simples').hide()
-            app.setAtributo('placa_estrangeira', 'false');
+            app.setAtributo('placa_estrangeira', false);
+            app.setAtributo('pais', null);
             
-            $("#grupo_identificacao_visual_avancar_simples").show();
+            $("#pais_simples option:contains('Selecione')").prop({selected: true});
+            $("select#pais_simples").selectmenu("refresh", true);
+            
+            $("#grupo_identificacao_visual_simples_avancar").show();
         });
         
         $('#pais_simples').change(function(){
             if(Number($(this).val()) != -1){
                 app.setAtributo('pais', $(this).val());
-                $("#grupo_identificacao_visual_avancar_simples").show();
+                $("#grupo_identificacao_visual_simples_avancar").show();
             } else {
-                $("#grupo_identificacao_visual_avancar_simples").hide();
+                $("#grupo_identificacao_visual_simples_avancar").hide();
                 app.setAtributo('pais', null);
             }
             
