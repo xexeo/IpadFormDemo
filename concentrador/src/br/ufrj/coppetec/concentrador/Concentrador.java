d41b2d63ac214d3105d2b9aebc45a924386c7ef6
ludes <ludes@192.168.0.103>
2016-04-11 01:34:31 -0300

    Initialization of the 'concentrador' project.

---
+++ concentrador/src/br/ufrj/coppetec/concentrador/Concentrador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufrj.coppetec.concentrador;
+
+/**
+ *
+ * @author ludes
+ */
+public class Concentrador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("br.ufrj.coppetec.concentrador.Concentrador.main()");
+        Janela janela = new Janela();
+        janela.setVisible(true);
+    }
+    
+}
