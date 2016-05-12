b7ee87be5215eee9146fd7bf1ae2539d507a0942
EduardoMangeli <eduardo@mangeli.com.br>
2016-05-12 16:35:27 -0300

    centralizing initial dialog

--- concentrador/src/br/ufrj/coppetec/concentrador/Concentrador.java
+++ concentrador/src/br/ufrj/coppetec/concentrador/Concentrador.java
@@ -58,6 +58,7 @@ public class Concentrador {
 		splash.setVisible(false);
 		
 		LoginJanela loginJanela = new LoginJanela(janela, true);
+		loginJanela.setLocationRelativeTo(null);
 		loginJanela.setVisible(true);
 		
 		
