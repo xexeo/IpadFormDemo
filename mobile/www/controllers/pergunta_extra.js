bc5a4f4cae68b2f7bb79a44cf4c7add035d08522
Mareas <mareas@gmail.com>
2017-09-04 10:36:05 -0300

    Ajuste duas perguntas extras para posto 149

--- mobile/www/controllers/pergunta_extra.js
+++ mobile/www/controllers/pergunta_extra.js
@@ -29,22 +29,28 @@ controllers.pergunta_extra = {
 										|| logins.user_logado.usr == '019t' || logins.user_logado.usr == '149t')) {
 			$("#grupo_pergunta_extra_p1").show();
 			util.inicializaSelect("select_pergunta_extra_p1", lista_opcoes);
+			if (logins.user_logado != null && (logins.user_logado.usr == '149' || logins.user_logado.usr == '149t')) {
+				util.inicializaSelect("select_pergunta_extra_p2", lista_opcoes);
+			}
 		}
 		else if (logins.user_logado != null && (logins.user_logado.usr == '040' || logins.user_logado.usr == '040t')) {
-			$("#grupo_pergunta_extra_p2").show();
-			util.inicializaSelect("select_pergunta_extra_p2", lista_opcoes);
+			$("#grupo_pergunta_extra_p3").show();
+			util.inicializaSelect("select_pergunta_extra_p3", lista_opcoes);
 		}
 	},
 
 	// Controla o show e hide dos elementos da tela
 	progressoTela : function() {
 
-		if (logins.user_logado != null && (logins.user_logado.usr == '019' || logins.user_logado.usr == '149'
-										|| logins.user_logado.usr == '019t' || logins.user_logado.usr == '149t')) {
+		if (logins.user_logado != null && (logins.user_logado.usr == '019' || logins.user_logado.usr == '019t')) {
 			util.progressoSelect("idPerguntaExtra", "select_pergunta_extra_p1", "grupo_pergunta_extra_avancar");
 		}
+		else if (logins.user_logado != null && (logins.user_logado.usr == '149' || logins.user_logado.usr == '149t')) {
+			util.progressoSelect("idPerguntaExtra", "select_pergunta_extra_p1", "grupo_pergunta_extra_p2");
+			util.progressoSelect("idPerguntaExtra2", "select_pergunta_extra_p2", "grupo_pergunta_extra_avancar");
+		}
 		else if (logins.user_logado != null && (logins.user_logado.usr == '040' || logins.user_logado.usr == '040t')) {
-			util.progressoSelect("idPerguntaExtra", "select_pergunta_extra_p2", "grupo_pergunta_extra_avancar");
+			util.progressoSelect("idPerguntaExtra", "select_pergunta_extra_p3", "grupo_pergunta_extra_avancar");
 		}
 		else {
 			// Não deveria entrar aqui. Mostra o botão avançar para finalizar a pesquisa e não ficar travado.
@@ -54,8 +60,18 @@ controllers.pergunta_extra = {
 
 	// Controla as validações dos componentes de tela após clicar em AVANÇAR
 	validar_componentes : function(id_avancar) {
-
-		return util.validaSelect("select_pergunta_extra", "da resposta");
+		if (logins.user_logado != null && (logins.user_logado.usr == '019' || logins.user_logado.usr == '019t')) {
+			return util.validaSelect("select_pergunta_extra_p1", "Pergunta Extra");
+		}
+		else if (logins.user_logado != null && (logins.user_logado.usr == '149' || logins.user_logado.usr == '149t')) {
+			var ok = util.validaSelect("select_pergunta_extra_p1", "Pergunta Extra 1");
+			ok = ok && util.validaSelect("select_pergunta_extra_p2", "Pergunta Extra 2");
+			return ok;
+		}
+		else if (logins.user_logado != null && (logins.user_logado.usr == '040' || logins.user_logado.usr == '040t')) {
+			return util.validaSelect("select_pergunta_extra_p3", "Pergunta Extra");
+		}
+		return false;
 	}
 
 };
