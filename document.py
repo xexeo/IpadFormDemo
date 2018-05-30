7a6f1e6a41d1b7720f724f3e9327397ac04058bf
Geraldo Xexeo <xexeo@cos.ufrj.br>
2018-05-30 18:15:42 -0300

    Documentação criada!

--- document.py
+++ document.py
@@ -40,6 +40,7 @@ FILE_FOOTER = """
 
 arquivos = [ "java", "js", "json" , "css", "html", "py","xml","properties",
             "txt"]
+
 nome = { "java" : "Java",
         "js" : "JavaScript",
         "json" : "JSON",
@@ -51,6 +52,7 @@ nome = { "java" : "Java",
         "txt": "txt"
         }
 
+# coisas que indicam em um nome de diretorio que não estamos interessados
 EXCLUDE = ["node_modules","doc","lib","doxy","resources","images","simple",
            ".settings",".external","concetrador\logs","concentrador\store",
            "ignore","plugins"]
@@ -59,8 +61,8 @@ EXCLUDE = ["node_modules","doc","lib","doxy","resources","images","simple",
 # Como queremos ordenar por tipo de arquivo
 
 def doublein(string,lista):
-    for str in lista:
-        if str in string:
+    for string1 in lista:
+        if string1 in string:
             return True
     return False
 
@@ -70,30 +72,43 @@ def trata(nome):
     return x.replace("\\","\\backslash ")
 
 
-
+#abre o arquivo de saída que receberá todo o código
 with open('output.latex','w',encoding="utf8") as fout:
     fout.writelines(HEADER)
+    # vai ordenar os arquivos por extensão
     for extensao in arquivos:
         print("EXTENSAO: ",extensao)
-        for root, subFolders, files in os.walk(rootdir):
-            
+        
+        # pega todos os arquivos a priori
+        for root, _, files in os.walk(rootdir):
+                
             for name in files:
  
+                
                 completo = os.path.join(root,name)
                 _,extatual = os.path.splitext(completo)
               
+                # não estamos interessados em coisas em alguns diretorios
                 if doublein(root,EXCLUDE):
                     continue
                 
+                # se estou na extensão que está sendo tratada agora
                 if extatual == "."+extensao :
+                    
                     fout.writelines(FILE_HEADER % trata(completo))
                     print(trata(completo))
+                    
+                    # tivemos muitos problemas de arquivo
                     try:
                         linha = 0
                         with open(completo, 'r',encoding="utf8") as fin:
                             for lines in fin:
+                                
                                 # defende de UTF-8 errados
+                                # aparecem em vários lugares devido ao uso
+                                # de vários sistemas
                                 line=bytes(lines,'utf-8').decode('utf-8','ignore')
+                                
                                 linha+=1
                                 if NUMERA:
                                     line="%4d:" % linha + line
@@ -101,7 +116,9 @@ with open('output.latex','w',encoding="utf8") as fout:
                     except:
                         print("arquivo de entrada: ",completo)
                         raise
+
                     fout.writelines(FILE_FOOTER)
+
     fout.writelines(FOOTER)
     
 
